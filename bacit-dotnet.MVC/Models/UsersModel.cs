9f0b7e70f23972d19647654d2b9a402418d2cac6
Sander-Aadal <89189052+Sander-Aadal@users.noreply.github.com>
2022-11-02 14:10:49 +0100

    Revert "TEST - Ikke ferdig stilt"

--- bacit-dotnet.MVC/Models/UsersModel.cs
+++ bacit-dotnet.MVC/Models/UsersModel.cs
@@ -12,18 +12,14 @@ namespace bacit_dotnet.MVC.Models
         public IEnumerable<Suggestion> Sug {
             get; set;
         }
-        public IEnumerable<team> Teams
-        {
-            get; set;
-        }
-    }
     }
 
     public class TeamModel
     {
-        public IEnumerable<team> teams
+        public IEnumerable<team> team
         {
             get; set;
         }
     }
+}
 
